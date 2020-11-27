48272697f9014c9741b9c99ca520e14bb9e0644c
Unyek <68945240+Unyek@users.noreply.github.com>
2020-11-27 20:50:00 +0700

    Add files via upload

---
+++ belajar-php/19_Surya.php
@@ -0,0 +1,18 @@
+<?php
+
+$data = array(
+
+	array("Nama"=>"Satria",
+		  "Kelas"=>"RPL 1"),
+
+	array("Nama"=>"Arsyidan",
+		  "Kelas"=>"RPL 2"),
+
+);
+
+echo "<pre>";
+print_r(&data);
+echo "</pre>";
+?>
+
+<?php
